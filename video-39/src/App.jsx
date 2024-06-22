4ece71f08fedee2523062a9a69f8eb63ce5fa3d7
AnshPatel-sh <patanshb@sheridancollege.ca>
2024-06-21 22:59:51 -0400

    spelling changes

--- video-39/src/App.jsx
+++ video-39/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   const [error, setError] = useState(null);
 
   /* The abort controller is used so that if the component is unmounted then the useEffect still can have a fetch 
-  request going on due to the function asynchronous behaviour so it should not update the state of the coponent 
+  request going on due to the function asynchronous behaviour so it should not update the state of the component 
   while the component is unmounted 
   
   because this can lead to memory leak
   so cleanup function that is the return fucntion runs in two conditions:
   .. first before the component unmounts so it cancels the network request
-  .. seconds before useEffect re-runs but hear it wont happen until the component is re-mounted again 
+  .. seconds before useEffect re-runs but here it wont happen until the component is re-mounted again 
   because empty array is passed */
 
   useEffect(() => {
